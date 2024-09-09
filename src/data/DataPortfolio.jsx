1ce75abe948fbbeae42c45ab1648caf2f8a3f7f5
itatyaa <chandraargajaya@gmail.com>
2024-09-09 09:39:53 +0700

    update

--- src/data/DataPortfolio.jsx
+++ src/data/DataPortfolio.jsx
@@ -4,42 +4,49 @@ import kasir from '../assets/kasir.png'
 import scratch from '../assets/scratch.png'
 import website from '../assets/website data siswa.png'
 import wishlist from '../assets/wishlist.png'
+import ecommerce from '../assets/e-commer.png'
 
 export const portfolioList = [
     {
         id: '1',
-        title: 'Projek 1',
+        title: 'Fanbook',
         image: fanbook,
-        skill: 'Saya membuat website fanbook'
+        skill: 'Saya membuat website fanbook, bertujuan agar user dapat mengunggah dan berbagi berbagai jenis konten seperti foto, video, fan art, dan artikel terkait topik atau idola yang mereka dukung'
     },
     {
         id: '2',
-        title: 'Projek 2',
+        title: 'Kalkulator',
         image: kalkulator,
         skill: 'Membuat kalkulator di scratch'
     },
     {
         id: '3',
-        title: 'Projek 3',
+        title: 'Kasir',
         image: kasir,
         skill: 'Membuat aplikasi kasir menggunakan java'
     },
     {
         id: '4',
-        title: 'Projek 4',
+        title: 'Space game',
         image: scratch,
         skill: 'Membuat space game di scratch'
     },
     {
         id: '5',
-        title: 'Projek 5',
+        title: 'data siswa',
         image: website,
-        skill: 'Membuat website data siswa menggunakan mysql dan laravel'
+        skill: 'Membuat website data siswa menggunakan mysql dan laravel, website ini bertujuan untuk menyimpan data siswa'
     },
     {
         id: '6',
-        title: 'Projek 6',
+        title: 'wishlist',
         image: wishlist,
-        skill: 'Membuat website wishlist'
+        skill: 'Membuat website wishlist untuk pengingat user menggunakan html css'
+    },
+    {
+        id: '7',
+        title: 'e commerce',
+        image: ecommerce,
+        skill: 'Membuat website ecommerce menggunakan react js dan mysql sebagai database nya'
     },
 ]
--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -48,7 +48,33 @@ function CV() {
                         iconStyle={{ background: '#f39c12', color: '#fff' }}
                         icon={<FaBriefcase />}
                     >                        <p>
-                            <strong>Saya menguasai java, laravel, html, css, bootstrap, c#, react, mysql. Selain itu saya cepat dalam belajar apapun</strong>
+                            <strong>Saya menguasai berbagai bahasa pemrograman. <br /></strong>
+                            <ul>
+                                <li>
+                                Java: Menguasai Java untuk pengembangan aplikasi berbasis desktop dan mobile, dengan kemampuan dalam pemrograman berorientasi objek serta pembuatan aplikasi yang efisien dan scalable
+                                </li><br />
+                                <li>
+                                Laravel: Berpengalaman menggunakan Laravel sebagai framework PHP untuk membangun aplikasi web yang kuat dan modular, dengan fitur seperti routing, middleware, dan ORM Eloquent.
+                                </li><br />
+                                <li>
+                                HTML: Menguasai HTML untuk pembuatan struktur dan konten web, memastikan bahwa markup yang digunakan sesuai standar dan semantik
+                                </li><br />
+                                <li>
+                                CSS: Terampil dalam penggunaan CSS untuk mempercantik tampilan web, menciptakan desain responsif dan user-friendly dengan pendekatan modern seperti Flexbox dan Grid
+                                </li><br />
+                                <li>
+                                Bootstrap: Berpengalaman dengan Bootstrap sebagai framework front-end untuk membangun layout web yang responsif dan cepat, memanfaatkan komponen UI yang telah tersedia untuk pengembangan yang lebih efisien
+                                </li><br />
+                                <li>
+                                C#: Menguasai C# dalam pengembangan aplikasi desktop dan game menggunakan framework seperti .NET dan Unity, dengan fokus pada pemrograman berorientasi objek dan pengembangan aplikasi yang performatif 
+                                </li><br />
+                                <li>
+                                React: Terampil dalam menggunakan React untuk membangun user interface yang dinamis dan interaktif, dengan pendekatan komponen berbasis JavaScript untuk pengembangan aplikasi web modern. <br />
+                                </li><br />
+                                <li>
+                                MySQL: Memiliki kemampuan dalam desain, pengelolaan, dan optimisasi database MySQL, memastikan performa query yang optimal dan data integrity.
+                                </li>
+                            </ul>
                         </p>
                     </VerticalTimelineElement>
                 </VerticalTimeline>
