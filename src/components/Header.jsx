55d9e91938627a93cf0a052df10a3b1159f3cf67
itatyaa <chandraargajaya@gmail.com>
2024-09-10 19:38:31 +0700

    pembaruan terbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ function Header() {
                 <div className='socialMedia'>
                     <a href='https://www.instagram.com/chandra_alfarel27'><FaInstagram /></a>
                     <a href='https://github.com/itatyaa'><FaGithub /></a>
-                    <a href='https://wa.me/085647884233'><FaWhatsapp /></a>
+                    <a href='https://wa.me/+6285647884233'><FaWhatsapp /></a>
                 </div>
             </div>
         </header>
